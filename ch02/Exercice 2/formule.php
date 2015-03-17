becec2c50f89cc8a3e43d7388cd1a48583f2c230
Nicolas <nicolas.lambiel@cpnv.ch>
2015-03-17 16:01:15 +0100

    test

---
+++ ch02/Exercice 2/formule.php
@@ -0,0 +1,9 @@
+<?php
+    $login = $_POST['login']; 
+    $MotDePasse = $_POST['MotDePasse']; 
+    $age = $_POST['age']; 
+    $sexe = $_POST['sexe']; 
+    $option = $_POST['option']; 
+    
+    echo "$login $MotDePasse $age $sexe $otion"
+?>
