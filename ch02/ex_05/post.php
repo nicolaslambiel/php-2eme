77016f928207c0fd81ae1ce0a7d647a71f9c437e
Nicolas <nicolas.lambiel@cpnv.ch>
2015-04-21 15:18:45 +0200

    Les exercices

---
+++ ch02/ex_05/post.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title></title>
+    <meta content="">
+    <style></style>
+  </head>
+  <body>
+      <form name="formulaire" method="post" action="traite.php">
+       Devinez un nombre entre 0 et 5:
+    <input name="nombre" type="text" size="1">
+    <br>
+    <input name="submit" type="submit" value="Envoyer">
+    </form>
+</html>
